407daa56fc8dc2150806f135e5bfecc9098ac87b
romshke <romshkeept+github@gmail.com>
2026-03-24 21:50:32 +0300

    Add Constants and val variables Topic

---
+++ Topics/Constants and val variables/Creating and printing a double value of an integer input/src/Main.kt
@@ -0,0 +1,16 @@
+// Start of the program
+fun main(args: Array<String>) {
+    /* The program reads an integer from the input. 
+     * Input a single integer.
+     */
+    val x = readln().toInt()
+
+    // Create a read-only (val) variable that equals twice of the input
+    // Your code here
+    val y = x * 2
+
+    // Print the value of the variable
+    // Your code here
+    print(y)
+}
+// End of the program
