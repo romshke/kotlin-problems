6f9ed41b5618321cd7363e251f170f6d551242ab
romshke <romshkeept+github@gmail.com>
2026-03-24 21:50:10 +0300

    Add Increment and decrement Topic

---
+++ Topics/Increment and decrement/Increment, then decrement an integer/src/Main.kt
@@ -0,0 +1,17 @@
+// import required to read input from stdin
+import java.util.Scanner
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+    // Read the input integer
+    var N = scanner.nextInt()
+
+    // TODO: Increment N by 1
+    N++
+
+    // TODO: Decrement the result by 2
+    N -= 2
+
+    // Print the final result
+    println(N)
+}
