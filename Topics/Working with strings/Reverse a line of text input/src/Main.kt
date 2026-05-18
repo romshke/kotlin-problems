d23ffaca5a5e25274f5dcfcb4fba6ee7aa3b2236
romshke <romshkeept+github@gmail.com>
2026-05-18 20:51:40 +0300

    Add Working with strings Topic

---
+++ Topics/Working with strings/Reverse a line of text input/src/Main.kt
@@ -0,0 +1,11 @@
+// In Kotlin, you can use the standard input/output using the "readLine() method for input and the print/println functions for output.
+
+fun main(args: Array<String>) {
+    // Step 1: Read a line of text from the standard input
+    val inputString = readLine()
+
+    // TODO: Write a function to reverse the input string
+
+    // Step 3: Print the reversed string using the println() function
+    print(inputString?.reversed())
+}
---
+++ Topics/Working with strings/The information about a person/src/Main.kt
@@ -0,0 +1,6 @@
+fun main() {
+    // put your code here
+    val parts = readln().split(' ')
+
+    print("${parts[0][0]}. ${parts[1]}, ${parts[2]} years old")
+}
